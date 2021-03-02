b8e5975ec01071b6ddb351c029ded1c0cac3083c
Michael Pingel <mdpingel@gmail.com>
2021-03-02 11:06:10 -0800

    write GetWidth_ReturnsParcelWidth test
    Co-authored-by: Jeroen van Seeters <vanseetersjeroen@gmail.com>

--- FictionalShipping.Tests/ModelTests/ParcelTests.cs
+++ FictionalShipping.Tests/ModelTests/ParcelTests.cs
@@ -9,7 +9,7 @@ namespace FictionalShipping.Tests
     [TestMethod]
     public void ParcelConstructor_CreatesInstanceOfParcel_Parcel()
     {
-      Parcel newParcel = new Parcel(0, 0);
+      Parcel newParcel = new Parcel(0, 0, 0);
       Assert.AreEqual(typeof(Parcel), newParcel.GetType());
     }
 
@@ -17,7 +17,7 @@ namespace FictionalShipping.Tests
     public void GetLength_ReturnsParcelLength_Int()
     {
       int length = 6;
-      Parcel newParcel = new Parcel(length, 0);
+      Parcel newParcel = new Parcel(length, 0, 0);
       int result = newParcel.Length;
       Assert.AreEqual(length, result);
     }
@@ -26,9 +26,18 @@ namespace FictionalShipping.Tests
     public void GetHeight_ReturnsParcelHeight_Int()
     {
       int height = 6;
-      Parcel newParcel = new Parcel(0, height);
+      Parcel newParcel = new Parcel(0, height, 0);
       int result = newParcel.Height;
       Assert.AreEqual(height, result);
+    }
+
+    [TestMethod]
+    public void GetWidth_ReturnsParcelWidth_Int()
+    {
+      int width = 6;
+      Parcel newParcel = new Parcel(0, 0, width);
+      int result = newParcel.Width;
+      Assert.AreEqual(width, result);
     }
   }
 }
