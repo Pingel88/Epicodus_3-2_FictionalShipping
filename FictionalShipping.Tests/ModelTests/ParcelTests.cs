bbd63cddc139a048e29619f1034d0f0473e812c0
Michael Pingel <mdpingel@gmail.com>
2021-03-02 11:00:11 -0800

    write GetHeight_ReturnsParcelHeight test
    Co-authored-by: Jeroen van Seeters <vanseetersjeroen@gmail.com>

--- FictionalShipping.Tests/ModelTests/ParcelTests.cs
+++ FictionalShipping.Tests/ModelTests/ParcelTests.cs
@@ -9,7 +9,7 @@ namespace FictionalShipping.Tests
     [TestMethod]
     public void ParcelConstructor_CreatesInstanceOfParcel_Parcel()
     {
-      Parcel newParcel = new Parcel(0);
+      Parcel newParcel = new Parcel(0, 0);
       Assert.AreEqual(typeof(Parcel), newParcel.GetType());
     }
 
@@ -17,9 +17,18 @@ namespace FictionalShipping.Tests
     public void GetLength_ReturnsParcelLength_Int()
     {
       int length = 6;
-      Parcel newParcel = new Parcel(length);
+      Parcel newParcel = new Parcel(length, 0);
       int result = newParcel.Length;
       Assert.AreEqual(length, result);
+    }
+
+    [TestMethod]
+    public void GetHeight_ReturnsParcelHeight_Int()
+    {
+      int height = 6;
+      Parcel newParcel = new Parcel(0, height);
+      int result = newParcel.Height;
+      Assert.AreEqual(height, result);
     }
   }
 }
