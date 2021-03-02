d21e6a89a6930e10c39d568958ed0cab50079cba
Michael Pingel <mdpingel@gmail.com>
2021-03-02 11:07:24 -0800

    good fail GetWidth_ReturnsParcelWidth test
    Co-authored-by: Jeroen van Seeters <vanseetersjeroen@gmail.com>

--- FictionalShipping/Models/Parcel.cs
+++ FictionalShipping/Models/Parcel.cs
@@ -4,11 +4,13 @@ namespace FictionalShipping.Models
   {
     public int Length { get; set; }
     public int Height { get; set; }
+    public int Width { get; set; }
 
-    public Parcel(int length, int height)
+    public Parcel(int length, int height, int width)
     {
       Length = length;
       Height = height;
+      Width = 0;
     }
   }
 }
